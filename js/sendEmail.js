0ead81bbbd2b56a7c904a2b0b5ad79e3eaed7ded
rhnmahajan83 <rhnmahajan83@gmail.com>
2020-07-03 13:37:13 +0530

    changed success message

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,27 +1,29 @@
-function sendSms() {
+function sendEmail() {
   console.log("Send Sms hit");
 
   bootstrap_alert = function () {}; //Alert Messages
   bootstrap_alert.warning = function (message) {
     $("#alert_placeholder").html(
-      '<div class="alert alert-success alert-dismissible">'+
-                      '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
-                      '<strong>Success! </strong>'+message+
-                    '</div>'
+      '<div class="alert alert-success alert-dismissible">' +
+        '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+        "<strong>Success! </strong>" +
+        message +
+        "</div>"
     );
   };
 
   function displaySuccessMessage() {
-    bootstrap_alert.warning("Email Sent successfully");
+    bootstrap_alert.warning("Message Sent");
   }
 
   bootstrap_error = function () {}; //Error Messages
   bootstrap_error.error = function (message) {
     $("#alert_placeholder").html(
-      '<div class="alert alert-danger alert-dismissible">'+
-                      '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
-                      '<strong>Error! </strong>'+message+
-                    '</div>'
+      '<div class="alert alert-danger alert-dismissible">' +
+        '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+        "<strong>Error! </strong>" +
+        message +
+        "</div>"
     );
   };
 
